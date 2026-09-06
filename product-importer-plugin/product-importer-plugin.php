b55269bb2689ef586fc5b0c18d2268f5b0367ef7
DODINHNAM <namdd.dev@gmail.com>
2026-09-06 16:28:23 +0700

    1.5.8

--- product-importer-plugin/product-importer-plugin.php
+++ product-importer-plugin/product-importer-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Product Importer Plugin
  * Description: A plugin to import products into WooCommerce with an easy-to-use interface.
- * Version: 1.5.7
+ * Version: 1.5.8
  * Author: NamDD
  */
 
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Define constants
-define( 'PIP_VERSION', '1.5.7' );
+define( 'PIP_VERSION', '1.5.8' );
 define( 'PIP_DIR', plugin_dir_path( __FILE__ ) );
 define( 'PIP_URL', plugin_dir_url( __FILE__ ) );
 define( 'PIP_PUBLIC_KEY', <<<EOD
